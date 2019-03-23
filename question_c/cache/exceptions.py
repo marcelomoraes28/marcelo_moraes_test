41839532aa1b40d576cdd2630fb26d9551651865
Marcelo Moraes <marcelomoraes@MacBook-Pro-de-Marcelo.local>
2019-03-23 16:31:18 -0300

    Written exceptions

---
+++ question_c/cache/exceptions.py
@@ -0,0 +1,12 @@
+class CacheException(Exception):
+    """
+    This exception is used only in base classes
+    """
+    pass
+
+
+class CacheClientException(Exception):
+    """
+    This exceptions is used only in Client module
+    """
+    pass
